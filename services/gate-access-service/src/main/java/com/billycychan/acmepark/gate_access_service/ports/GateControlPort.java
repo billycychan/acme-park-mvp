b7238dd3e89159fa39e23733b17a6d50b1131a05
Billy Chan <billycychan.work@gmail.com>
2024-10-07 16:22:15 -0400

    chore: remove the RPC implemntation

--- services/gate-access-service/src/main/java/com/billycychan/acmepark/gate_access_service/ports/GateControlPort.java
+++
@@ -1,7 +0,0 @@
-package com.billycychan.acmepark.gate_access_service.ports;
-
-import org.springframework.context.annotation.Bean;
-
-public interface GateControlPort {
-    void openGate(String parkingLot, String gate);
-}
--- services/gate-access-service/src/main/java/com/billycychan/acmepark/gate_access_service/ports/PermitValidationPort.java
+++
@@ -1,8 +0,0 @@
-package com.billycychan.acmepark.gate_access_service.ports;
-
-import com.billycychan.acmepark.gate_access_service.dto.TransponderAccessRequest;
-
-public interface PermitValidationPort {
-    void isPermitValid(TransponderAccessRequest transponderAccessRequest);
-}
-
