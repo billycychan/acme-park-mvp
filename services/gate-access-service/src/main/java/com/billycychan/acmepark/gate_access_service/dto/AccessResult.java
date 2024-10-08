f21232fb1632c8dd17e5b305500c10043722d3d7
Billy Chan <billycychan.work@gmail.com>
2024-10-08 11:57:20 -0400

    feat: add docker deployment

--- services/gate-access-service/src/main/java/com/billycychan/acmepark/gate_access_service/dto/AccessResult.java
+++ services/gate-access-service/src/main/java/com/billycychan/acmepark/gate_access_service/dto/AccessResult.java
@@ -6,12 +6,21 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@NoArgsConstructor
-@Data
+
+@NoArgsConstructor @Data
 public class AccessResult implements Serializable {
     @JsonProperty("transponder-id") String transponderId;
     @JsonProperty("gate") String gate;
     @JsonProperty("lot") String lot;
     @JsonProperty("event") PermitValidatedEvent event;
     @JsonProperty("message") String message ;
+
+
+    public AccessResult(String transponderId, String gate, String lot, PermitValidatedEvent event, String message) {
+        this.transponderId = transponderId;
+        this.gate = gate;
+        this.lot = lot;
+        this.event = event;
+        this.message = message;
+    }
 }
